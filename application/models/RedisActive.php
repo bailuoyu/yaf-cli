321e888fbd735ee76e6907769289e57af0ebfac0
白洛羽 <1171621177@qq.com>
2019-05-29 11:29:32 +0800

    Update RedisActive.php

--- application/models/RedisActive.php
+++ application/models/RedisActive.php
@@ -42,21 +42,37 @@ abstract class RedisActiveModel {
         return $expire;
     }
     
-    //设置时效
-    public function setExpire($key,$attr_key){
-        if(strstr($key,$this->key_pre.':')){
-            throw new \Error('请不要使用redis真实键名!');
+    /*
+     * 设置时效
+     * 兼容拼接key和真实key
+     * 真实key时会忽略$attr_key
+     */
+    public function setExpire($key,$attr_key=null){
+        if(substr_compare($key,$this->key_pre.':',0,strlen($this->key_pre)+1)==0){    //***如果是真实键名
+            $redis_key = $key;
+            $str = ltrim($key,$this->key_pre.':');  //去除key_pre
+            $attr_key = strstr($str,':',true);
+            if(!$attr_key){
+                $attr_key = $str;
+            }
+            if(empty($this->keyr[$attr_key])){     //如果键类型不存在
+                throw new \Error('非法的attribute!');
+            }else{
+                $r = $this->keyr[$attr_key];
+            }
+            $expire = $r[1];
+        }else{  //***如果是拼接键名
+            if(empty($this->keyr[$attr_key])){     //如果键类型不存在
+                throw new \Error('非法的attribute!');
+            }else{
+                $r = $this->keyr[$attr_key];
+            }
+            $redis_key = $this->key_pre.':'.$r[0];
+            $expire = $r[1];
+            if ($key){
+                $redis_key .= ':'.$key;
+            }
         }
-        if(empty($this->keyr[$attr_key])){     //如果键类型不存在
-            throw new \Error('非法的attribute!');
-        }else{
-            $r = $this->keyr[$attr_key];
-        }
-        $redis_key = $this->key_pre.':'.$r[0];
-        if ($key){
-            $redis_key .= ':'.$key;
-        }
-        $expire = $r[1];
         return $this->redis -> expire($redis_key,$expire);
     }
     
